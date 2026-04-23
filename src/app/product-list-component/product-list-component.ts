009cdb8240af8c90275153df240999198e42408c
AnissaBrihmat <121158683+AnissaBrihmat@users.noreply.github.com>
2026-04-23 14:05:28 +0200

    Ajout du ProductComponent et intégration dans la liste des produits

--- src/app/product-list-component/product-list-component.ts
+++ src/app/product-list-component/product-list-component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { Product } from '../models/products';
+import { ProductComponent } from '../product-component/product-component';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'app-product-list-component',
   standalone: true,
+  imports: [ProductComponent],
   templateUrl: './product-list-component.html',
-  styleUrl: './product-list-component.scss'
+  styleUrl: './product-list-component.scss',
 })
 export class ProductListComponent {
 
   produits: Product[] = [
-    new Product(1, 'Peluche', 35),
-    new Product(2, 'Voiture', 49),
-    new Product(3, 'Lego', 25)
+    new Product(1, 'Peluche', 35, 'https://...'),
+    new Product(2, 'Voiture', 49, 'https://...')
   ];
-
 }
