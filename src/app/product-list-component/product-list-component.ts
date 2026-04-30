b9ba3c4c73fe763ccc3c99cc95a6eea69dd6343a
AnissaBrihmat <121158683+AnissaBrihmat@users.noreply.github.com>
2026-04-30 14:04:00 +0200

    Ajout de la pagination des produits

--- src/app/product-list-component/product-list-component.ts
+++ src/app/product-list-component/product-list-component.ts
@@ -1,79 +1,85 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Product } from '../models/products';
 import { ProductComponent } from '../product-component/product-component';
 import { CartService } from '../services/cart-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list-component',
   standalone: true,
-  imports: [ProductComponent, FormsModule], // permet d'utiliser ngModel
+  imports: [ProductComponent, FormsModule],
   templateUrl: './product-list-component.html',
   styleUrl: './product-list-component.scss',
 })
 export class ProductListComponent {
 
-  // liste des produits
+  // PRODUITS
   produits: Product[] = [
-    new Product(
-      1,
-      'Peluche',
-      35,
-      10,
-      'https://...',
-      1,
-      'Jouets'
-    ),
-
-    new Product(
-      2,
-      'Voiture',
-      49,
-      38,
-      'https://...',
-      1,
-      'Jouets'
-    ),
-
-    new Product(
-      3,
-      'Laptop',
-      10,
-      30,
-      'https://...',
-      1,
-      'Tech'
-    )
+    new Product(1, 'Peluche', 35, 10, 'https://...', 1, 'jouet'),
+    new Product(2, 'Voiture', 49, 38, 'https://...', 1, 'jouet'),
+    new Product(3, 'Laptop', 10, 30, 'https://...', 1, 'tech'),
+    new Product(4, 'Tablette', 200, 15, 'https://...', 1, 'tech'),
+    new Product(5, 'Clavier', 50, 12, 'https://...', 1, 'tech'),
+    new Product(6, 'Souris', 30, 20, 'https://...', 1, 'tech')
   ];
 
-  // service panier
-  constructor(private cartService: CartService) {}
+  // FILTRE
+  selectedCategory: string = 'Tous';
+  categories: string[] = ['Tous', 'jouet', 'tech'];
 
-  // texte de recherche tapé par l'utilisateur
+  // RECHERCHE
   searchText: string = '';
 
-  // catégorie sélectionnée par défaut
-  selectedCategory: string = 'Tous';
+  // PAGINATION
+  currentPage: number = 1;
+  itemsPerPage: number = 2;
 
-  // liste des catégories
-  categories: string[] = ['Tous', 'Jouets', 'Tech', 'Maison'];
+  constructor(private cartService: CartService) {}
 
-  // ajout d’un produit au panier
+  // AJOUT PANIER
   onAddToCart(product: Product) {
     this.cartService.addToCart(product);
-    console.log('Panier:', this.cartService.getCart());
   }
 
-  // filtre les produits par catégorie et recherche
-  get filteredProducts() {
-    return this.produits.filter(
-      p =>
-        // filtre par catégorie
-        (this.selectedCategory === 'Tous' ||
-          p.category === this.selectedCategory) &&
+  // FILTRAGE + RECHERCHE
+  get filteredProducts(): Product[] {
+    return this.produits.filter((p: Product) => {
 
-        // filtre par texte saisi
-        p.nom.toLowerCase().includes(this.searchText.toLowerCase())
-    );
+      const matchCategory =
+        this.selectedCategory === 'Tous' ||
+        p.category === this.selectedCategory;
+
+      const matchSearch =
+        p.nom.toLowerCase().includes(this.searchText.toLowerCase());
+
+      return matchCategory && matchSearch;
+    });
+  }
+
+  // PAGINATION
+  get paginatedProducts(): Product[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+
+    return this.filteredProducts.slice(start, end);
+  }
+
+  // PAGE SUIVANTE
+  nextPage() {
+    if (this.currentPage * this.itemsPerPage < this.filteredProducts.length) {
+      this.currentPage++;
+    }
+  }
+
+  // PAGE PRECEDENTE
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  // RESET PAGE (utile filtre/recherche)
+  resetPage() {
+    this.currentPage = 1;
   }
 }
