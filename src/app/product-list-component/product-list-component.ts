65fd94ff9ccd50f2643346d6da45f76175b2c246
AnissaBrihmat <121158683+AnissaBrihmat@users.noreply.github.com>
2026-04-30 09:17:47 +0200

    Ajout de la fonctionnalité de recherche des produits

--- src/app/product-list-component/product-list-component.ts
+++ src/app/product-list-component/product-list-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Product } from '../models/products';
 import { ProductComponent } from '../product-component/product-component';
 import { CartService } from '../services/cart-service';
@@ -6,62 +7,73 @@ import { CartService } from '../services/cart-service';
 @Component({
   selector: 'app-product-list-component',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, FormsModule], // permet d'utiliser ngModel
   templateUrl: './product-list-component.html',
   styleUrl: './product-list-component.scss',
 })
 export class ProductListComponent {
 
+  // liste des produits
   produits: Product[] = [
-  new Product(
-    1,
-    'Peluche',
-    35,
-    10,
-    'https://...',
-    1,
-    'Jouets'
-  ),
+    new Product(
+      1,
+      'Peluche',
+      35,
+      10,
+      'https://...',
+      1,
+      'Jouets'
+    ),
 
-  new Product(
-    2,
-    'Voiture',
-    49,
-    38,
-    'https://...',
-    1,
-    'Jouets'
-  ),
+    new Product(
+      2,
+      'Voiture',
+      49,
+      38,
+      'https://...',
+      1,
+      'Jouets'
+    ),
 
-  new Product(
-    3,
-    'laptop',
-    10,
-    30,
-    'https://...',
-    1,
-    'Tech'
-  )
-];
-   //  injection du service panier
+    new Product(
+      3,
+      'Laptop',
+      10,
+      30,
+      'https://...',
+      1,
+      'Tech'
+    )
+  ];
+
+  // service panier
   constructor(private cartService: CartService) {}
 
-  // ajout au panier
+  // texte de recherche tapé par l'utilisateur
+  searchText: string = '';
+
+  // catégorie sélectionnée par défaut
+  selectedCategory: string = 'Tous';
+
+  // liste des catégories
+  categories: string[] = ['Tous', 'Jouets', 'Tech', 'Maison'];
+
+  // ajout d’un produit au panier
   onAddToCart(product: Product) {
     this.cartService.addToCart(product);
     console.log('Panier:', this.cartService.getCart());
   }
-  selectedCategory: string = 'Tous';
 
-categories: string[] = ['Tous', 'Jouets', 'Tech', 'Maison'];
+  // filtre les produits par catégorie et recherche
+  get filteredProducts() {
+    return this.produits.filter(
+      p =>
+        // filtre par catégorie
+        (this.selectedCategory === 'Tous' ||
+          p.category === this.selectedCategory) &&
 
-get filteredProducts() {
-  if (this.selectedCategory === 'Tous') {
-    return this.produits;
+        // filtre par texte saisi
+        p.nom.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
-
-  return this.produits.filter(
-    p => p.category === this.selectedCategory
-  );
-}
 }
